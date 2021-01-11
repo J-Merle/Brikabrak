4bf41e10dca544602bd211934702fd1585eb666f
Jeremy Merle <jeremy.merle@protonmail.com>
2021-01-11 22:05:42 +0100

    Initial commit

---
+++ Brikabrak/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello wolrld!" << std::endl;
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Quit();
+
+  return 0;
+
+}
---
+++ premake5.lua
@@ -0,0 +1,42 @@
+workspace "Brikabrak"
+  configurations {"Debug", "Release"}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Brikabrak"
+  location "Brikabrak"
+  kind "ConsoleApp"
+  architecture "x64"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs {
+    "./%{prj.name}/vendor/SDL2/include"
+  }
+
+  
+
+  libdirs { 
+    "%{prj.name}/vendor/SDL2/lib"
+  }
+
+  links {"SDL2"}
+
+  filter "system:linux"
+    cppdialect "C++17"
+
+  filter "configurations:Debug"
+    symbols "on"
+    defines "DEBUG"
+
+  filter "configurations:Release"
+    optimize "on"
+    defines "NDEBUG"
